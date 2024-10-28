c5615059597b8437f7c6dacb06e0daf744192705
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-10-28 09:25:54 +0800

    kernel: apk_sign: add more size/hash pairs
    this will make it support
    - this repo's manager
    - official manager, but I guess up to 1.0.1 for non-gki
    - 5ec1cff's MKSU
    - KernelSU NEXT
    - rsuntk's MKSU
    - SukiSU-Ultra

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -318,5 +318,10 @@ bool is_manager_apk(char *path)
 {
 	return (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)  // KernelSU official
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588") // KOWX712/KernelSU
+	|| check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // backslashxx/KernelSU
+	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")  // 5ec1cff/KernelSU
+	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rsuntk/KernelSU
+	|| check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")  // rifsxd/KernelSU-Next
+	|| check_v2_signature(path, 0x35c, "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef")  // ShirkNeko/SukiSU-Ultra
 	);
 }
