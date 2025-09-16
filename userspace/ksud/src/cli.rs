0c4105db463f55ccee5416a7df09ab5c1da3d887
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-16 14:07:57 +0800

    ksud: add cmd for nuke-ext4-sysfs
    as title, this is the exact same shit as the umount one
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- userspace/ksud/src/cli.rs
+++ userspace/ksud/src/cli.rs
@@ -36,6 +36,11 @@ enum Commands {
         path: PathBuf,  
     },
 
+    /// Nuke custom ext4 sysfs path
+    NukeExt4Sysfs {
+        path: PathBuf,
+    },
+
     /// Trigger `post-fs-data` event
     PostFsData,
 
@@ -329,6 +334,22 @@ pub fn run() -> Result<()> {
 		    let mut dummy: u32 = 0;  
 		    unsafe {  
 		        libc::prctl(0xDEADBEEFu32 as i32, 10001, c_path.as_ptr() as libc::c_ulong,   
+		                   &mut dummy as *mut u32 as libc::c_ulong,   
+		                   &mut dummy as *mut u32 as libc::c_ulong);  
+		    }  
+		}  
+		std::result::Result::Err(e) => {  
+		    eprintln!("Failed to create CString: {}", e);  
+		}  
+	    }  
+	    Ok(())  
+	}
+	Commands::NukeExt4Sysfs { path } => {  
+	    match CString::new(path.to_string_lossy().as_ref()) {  
+		std::result::Result::Ok(c_path) => {  
+		    let mut dummy: u32 = 0;  
+		    unsafe {  
+		        libc::prctl(0xDEADBEEFu32 as i32, 10002, c_path.as_ptr() as libc::c_ulong,   
 		                   &mut dummy as *mut u32 as libc::c_ulong,   
 		                   &mut dummy as *mut u32 as libc::c_ulong);  
 		    }  
