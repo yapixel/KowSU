bd779d3095c811f564828dccbe9e1999160ebef6
KOWX712 <leecc0503@gmail.com>
2025-09-21 17:47:15 +0800

    Revert switch ui to miuix design style
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/KernelSUApplication.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/KernelSUApplication.kt
@@ -2,6 +2,10 @@ package me.weishu.kernelsu
 
 import android.app.Application
 import android.system.Os
+import coil.Coil
+import coil.ImageLoader
+import me.zhanghai.android.appiconloader.coil.AppIconFetcher
+import me.zhanghai.android.appiconloader.coil.AppIconKeyer
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import java.io.File
@@ -16,6 +20,17 @@ class KernelSUApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ksuApp = this
+
+        val context = this
+        val iconSize = resources.getDimensionPixelSize(android.R.dimen.app_icon_size)
+        Coil.setImageLoader(
+            ImageLoader.Builder(context)
+                .components {
+                    add(AppIconKeyer())
+                    add(AppIconFetcher.Factory(iconSize, false, context))
+                }
+                .build()
+        )
 
         val webroot = File(dataDir, "webroot")
         if (!webroot.exists()) {
@@ -35,4 +50,6 @@ class KernelSUApplication : Application() {
                     )
                 }.build()
     }
+
+
 }
